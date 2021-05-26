b67bb470e995bf0f23fc701c450c78f2e0e416dd
lurenhua <1472638@qq.com>
2021-05-26 20:19:38 +0800

    删除公司的公共包pests，合并到common中

---
+++ app/src/main/java/com/aiton/pestscontrolandroid/utils/BugTest.java
@@ -0,0 +1,12 @@
+package com.aiton.pestscontrolandroid.utils;
+
+import android.util.Log;
+
+public class BugTest {
+
+    public  String bug(){
+//        String str = null;
+//        Log.e("TAG", "bug: "+str.length());
+        return "this is a bug test";
+    }
+}
